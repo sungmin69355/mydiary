f009875e369d4311f1dc55afe31e553268b065d4
sungmin69355 <sungmin69355@naver.com>
2020-08-14 14:20:03 +0900

    update부분  주석 추가

--- project/mydiary/urls.py
+++ project/mydiary/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('create',views.create,name='create'),
     path('detail/<int:posts_id>/',views.detail,name='detail'), 
     path('delete/<int:posts_id>/',views.delete,name='delete'),
-    path('update/<int:posts_id>/',views.update,name='update'),#여기 수정중...
+    path('update/<int:posts_id>/',views.update,name='update'),#여기 수정중... html에서 값을 post 값을 넘긴다음 그 넘긴 값으로 업로드를해줘야함
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
