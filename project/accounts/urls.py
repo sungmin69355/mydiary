12232fc84607eb3610b088788fc0d0d9e1df6bdb
sungmin69355 <sungmin69355@naver.com>
2020-08-20 17:32:30 +0900

    비밀번호수정할수있도록 구현중

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signup/',views.signup, name='signup'),
     path('login/',views.login, name='login'),
     path('logout/',views.logout_request, name='logout'),
+    path('change_password_page/',views.change_password_page, name ='change_password_page')
 ]
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth #내부 유저모델 사용
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
     if request.method  == 'POST':
@@ -37,3 +39,29 @@ def kakao_login(request):
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={'45f14f583e184b0b0b976cf5ef4b3095'}&redirect_uri={'home'}&response_type=code"
     )
+##비밀번호 변경란 수정중....
+
+def change_password_page(request):
+    return render(request, "change_pw.html")
+    
+@login_required
+def change_password(request):
+    context= {}
+    if request.method == "POST":
+        current_password = request.POST.get("origin_password")
+        user = request.user
+        if check_password(current_password,user.password):
+            new_password = request.POST.get("password1")
+            password_confirm = request.POST.get("password2")
+            if new_password == password_confirm:
+                user.set_password(new_password)
+                user.save()
+                auth.login(request,user)
+                return redirect("home")
+            else:
+                context.update({'error':"새로운 비밀번호를 다시 확인해주세요."})
+    else:
+        context.update({'error':"현재 비밀번호가 일치하지 않습니다."})
+
+    return render(request, "change_pw.html",context)
+
